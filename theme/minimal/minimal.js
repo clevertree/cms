881b897652f42a92d2394bc618a8b44cc7a2803c
Ari Asulin <ari.asulin@gmail.com>
2018-12-31 10:42:48 -0700

    hash navigation

--- theme/minimal/minimal.js
+++ theme/minimal/minimal.js
@@ -3,10 +3,11 @@
 
     function onHashChange(e) {
         const hash = window.location.hash.substr(1);
-        console.log(hash, e);
-        document.querySelectorAll('article.page-article').forEach(function(articleElm) {
-            articleElm.classList.toggle('selected', articleElm.getAttribute('id') === hash);
-        })
+        if(hash) {
+            document.querySelectorAll('article.page-article').forEach(function (articleElm) {
+                articleElm.classList.toggle('selected', articleElm.getAttribute('id') === hash);
+            })
+        }
     }
     document.addEventListener('DOMContentLoaded', onHashChange);
 })()
