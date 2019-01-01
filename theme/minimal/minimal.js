840c6dc94d25f71a85d3bffde498b832db5edf02
Ari Asulin <ari.asulin@gmail.com>
2018-12-31 20:38:26 -0700

    opacity transition. new content

--- theme/minimal/minimal.js
+++ theme/minimal/minimal.js
@@ -5,8 +5,11 @@
         const hash = window.location.hash.substr(1);
         if(hash) {
             document.querySelectorAll('article.page-article').forEach(function (articleElm) {
-                articleElm.classList.toggle('selected', articleElm.getAttribute('id') === hash);
-            })
+                articleElm.classList.toggle('selected', articleElm.getAttribute('data-path') === hash);
+            });
+            if(!document.querySelector('article.page-article.selected'))
+                document.querySelector('article.page-article-default').classList.toggle('selected');
+            e.preventDefault();
         }
     }
     document.addEventListener('DOMContentLoaded', onHashChange);
