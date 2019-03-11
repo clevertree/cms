52704e84c349ce52dd6c5926bce1f6cb6f48106b
Ari Asulin <ari.asulin@gmail.com>
2019-03-11 16:55:40 -0700

    server autoconfiguration fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { HTTPServer } = require('./http/http.server');
 (async () => {
     if(process && process.argv && process.argv.indexOf('--configure') !== -1) {
         console.log("Starting configuration");
-        await ConfigManager.configure();
+        await ConfigManager.configure(true);
     }
 
 
